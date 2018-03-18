fd37b5e8a7fa3afae2949a26b833d711cb8e4534
juan <troyasoft1642@gmail.com>
2018-03-18 14:39:50 -0600

    Se agrega esquema con bluetooth para android

--- CodigoArduino/Sensor de Distancia/SensorDistancia.ino
+++ CodigoArduino/Sensor de Distancia/SensorDistancia.ino
@@ -1,19 +1,15 @@
 /**
-   GeekFactory - "Construye tu propia tecnologia"
-   Distribucion de materiales para el desarrollo e innovacion tecnologica
-   www.geekfactory.mx
-
    EJEMPLO SENSOR ULTRASONICO 1
-
    REALIZA LA MEDICION DE DISTANCIA CON UN SENSOR ULTRASÓNICO HC-SR04 CONECTADO
    AL ARDUINO. ESTE EJEMPLO NO UTILIZA LIBRERIAS ESPECIALES PARA EL SENSOR, SOLAMENTE
    SE UTILIZA LA FUNCION "PULSEIN" PARA MEDIR LA LONGITUD DEL PULSO DE ECO.
-
 */
-// DECLARACION DE VARIABLES PARA PINES
+
+//variables
 const int pinecho = 8;
 const int pintrigger = 9;
 const int pinled = 13;
+String envia="";
 
 // VARIABLES PARA CALCULOS
 unsigned int tiempo, distancia;
@@ -37,22 +33,18 @@ void loop() {
   digitalWrite(pintrigger, LOW);
 
   // MEDIR EL TIEMPO EN ESTADO ALTO DEL PIN "ECHO" EL PULSO ES PROPORCIONAL A LA DISTANCIA MEDIDA
-  tiempo = pulseIn(ECHO, HIGH);
+  tiempo = pulseIn(11, HIGH);
 
   // LA VELOCIDAD DEL SONIDO ES DE 340 M/S O 29 MICROSEGUNDOS POR CENTIMETRO
   // DIVIDIMOS EL TIEMPO DEL PULSO ENTRE 58, TIEMPO QUE TARDA RECORRER IDA Y VUELTA UN CENTIMETRO LA ONDA SONORA
   distancia = tiempo / 58;
 
-  // ENVIAR EL RESULTADO AL MONITOR SERIAL
-  Serial.print(distancia);
-  Serial.println(" cm");
-  delay(200);
-
-  // ENCENDER EL LED CUANDO SE CUMPLA CON CIERTA DISTANCIA
-  if (distancia <= 15) {
-    digitalWrite(13, HIGH);
-    delay(500);
-  } else {
-    digitalWrite(13, LOW);
-  }
+  //enviamos los datos a "Serial Sensor Plot " ya se android o Windows funciona para ambos 
+  //agregamos la distancia 
+  envia.concat(distancia);
+  envia.concat(" ");//espacio al final
+  //se envia via Serial
+  Serial.println(envia);
+  envia="";
+  delay(300);//el tiempo debe ser igual en android o windows  
 }
